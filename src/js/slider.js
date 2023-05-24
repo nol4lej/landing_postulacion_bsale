e40be2ca76a598a71671a1ee9affd66a3c7c4f7a
nol4lej <nome.ahumada@outlook.com>
2023-05-24 00:50:27 -0400

    Slick rounded slider complete

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,18 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let slides = document.querySelectorAll(".slide");
-    let slicks_rounded = document.querySelectorAll("slick_rounded");
-    let currentSlide = 0;
-    let currentSlick = 0;
-    const slideInterval = setInterval(nextSlide, 5000);
-  
-    function nextSlide() {
-      slides[currentSlide].classList.remove("active");
-      currentSlide = (currentSlide + 1) % slides.length;
-      slides[currentSlide].classList.add("active");
+let slicks_rounded = document.querySelectorAll(".slick_rounded");
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+const slideInterval = setInterval(nextSlide, 5000);
 
-      slicks_rounded[currentSlick].classList.remove("active");
-      currentSlick = (currentSlick + 1) % slicks_rounded.length;
-      slicks_rounded[currentSlick].classList.add("active");
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  slicks_rounded[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+  slicks_rounded[currentSlide].classList.add("active");
+}
+
+function selectSlick(target){
+  for(let i = 0; i < slicks_rounded.length; i++){
+    if(slicks_rounded[i].classList.contains("active") && slides[i].classList.contains("active")){
+      slicks_rounded[i].classList.remove("active");
+      slides[i].classList.remove("active")
     }
-});
-  
+  }
+  slicks_rounded[target].classList.add("active");
+  slides[target].classList.add("active");
+  currentSlide = parseInt(target); // actualizamos al target actual 
+  console.log(target)
+}
+
+slicks_rounded.onclick = selectSlick;
