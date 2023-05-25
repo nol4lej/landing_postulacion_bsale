0d23dc4d9bd0debdb46215578a8782983ab7ebe9
nol4lej <nome.ahumada@outlook.com>
2023-05-25 02:26:05 -0400

    Navbar full controlled

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,6 +2,16 @@ const button = document.getElementById("nav_hamburger_button");
 const menu = document.getElementById("nav_cont_menu");
 
 button.addEventListener("click", () => {
+    menu.style.transition = "height 0.3s ease, opacity 0.3s ease";
     // menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
     menu.classList.toggle('active');
+})
+
+window.addEventListener("resize", () => {
+    if(innerWidth > 768){
+        menu.style.transition = "none";
+    }
+    if(menu.classList.contains = "active"){
+        menu.classList.remove("active");
+    }
 })
